9b88f3ef845b8bd2aa093bc654780154d241f489
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-26 10:24:28 +1000

    add typing status and readme

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Tutorial
+
+click ![here](https://dev.to/novu/building-a-chat-app-with-socketio-and-react-2edj) for the tutorial
--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 const ChatFooter = ({ socket }) => {
   const [message, setMessage] = useState("");
 
+  const handleTyping = () =>
+    socket.emit("typing", `${localStorage.getItem("userName")} is typing`);
+
   const handleSendMessage = (e) => {
     e.preventDefault();
     if (message.trim() && localStorage.getItem("userName")) {
@@ -15,7 +18,6 @@ const ChatFooter = ({ socket }) => {
     }
     setMessage("");
   };
-
   return (
     <div className="chat__footer">
       <form className="form" onSubmit={handleSendMessage}>
@@ -25,6 +27,7 @@ const ChatFooter = ({ socket }) => {
           className="message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleTyping}
         />
         <button className="sendBtn">SEND</button>
       </form>
